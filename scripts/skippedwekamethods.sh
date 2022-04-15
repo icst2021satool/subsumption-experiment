ab38d6e5402ef48f5460ab3a8dec6dc8c489b80b
icst2021satool <icst2021satool@gmail.com>
2022-04-15 20:36:20 -0300

    Fixed directory with nodesubsume.csv file

--- scripts/skippedwekamethods.sh
+++ scripts/skippedwekamethods.sh
@@ -2,6 +2,7 @@
 
 # Shell to find methods whose data were not used in Weka
 
-./comparison.py ~/experimentos/subsumption-experiment/jaguar-data/Weka/output/weka/ ~/experimentos/subsumption-experiment/subsumption-data/Weka/ \
+./comparison.py ../jaguar-data/Weka/output/weka/ ../subsumption-data/Weka/ \
 | grep NOK | awk -F ":" '{print $1}' > skippedwekaclasses.txt
-for v in `cat skippedwekaclasses.txt `; do cat ~/experimentos/subsumption-experiment/subsumption-data/nodesubsume.csv | grep $v; done | wc
+for v in `cat skippedwekaclasses.txt `; do cat ../subsumption-data/checks-output/allprograms/nodesubsume.csv | grep $v; done | wc
+rm -f skippedwekaclasses.txt
