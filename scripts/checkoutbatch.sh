46f42777a698c4c345b2bebed5da681992c8321d
icst2021satool <icst2021satool@gmail.com>
2022-04-15 20:22:42 -0300

    Added checkout of Lang, Mockito, and the correct version of Collections

--- scripts/checkoutbatch.sh
+++ scripts/checkoutbatch.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -x
-Identifiers="Chart Cli Closure Codec Collections Compress Csv Gson JacksonCore JacksonDatabind JacksonXml Jsoup JxPath Math Time"
+Identifiers="Chart Cli Closure Codec Compress Csv Gson JacksonCore JacksonDatabind JacksonXml Jsoup JxPath Lang Math Mockito Time"
 
 for project in $Identifiers
 do
   echo $project
   ./checkout_project.sh $project 1b
 done
+
+./checkout_project.sh Collections 25b
